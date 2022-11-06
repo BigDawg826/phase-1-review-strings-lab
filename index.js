e19b167125de30e275b052495a7f74cc358ce293
Darian Fultz <dfultz826@yahoo.com>
2022-11-06 12:45:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0,1) + '!'}`;
-
+// testing code
+// console.log(currentUser);
+// console.log(welcomeMessage);
+// console.log(excitedWelcomeMessage);
+// console.log(shortGreeting);
